a65f59d5ea6821e17deb8091d8eef6ae91df9f8a
lizdenhup <lizdenhup@gmail.com>
2015-12-06 21:51:38 -0600

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,18 @@
 # Enter your procedural solution here!
+def largest_prime_factor(limit)
+  max = 0
+  (2..Math.sqrt(limit)).each do |j| 
+    if limit % j == 0 && is_prime?(j) && j > max 
+      max = j
+    end 
+  end 
+  max 
+end
+
+def is_prime?(limit)
+  if (limit == 2 || limit == 3 || limit % 6 == 1  || limit % 6 == 5)
+    (2...limit).all? { |i| limit % i != 0 }
+  else
+    false # ie not prime
+  end 
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor 
+
+  def initialize(number)
+    @number = number
+  end
+
+def number
+  max = 0
+  (2..Math.sqrt(@number)).each do |j| 
+    if @number % j == 0 && is_prime?(j) && j > max 
+      max = j
+    end 
+  end 
+  max 
+end
+
+def is_prime?(j)
+  if (j == 2 || j == 3 || j % 6 == 1  || j % 6 == 5)
+    (2...j).all? { |i| j % i != 0 }
+  else
+    false # ie not prime
+  end 
+end
+
+end 
